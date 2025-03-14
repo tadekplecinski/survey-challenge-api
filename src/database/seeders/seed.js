1a5df29844437b82337bc1db23dd28fa39db9501
tadek <tadekpl@gmail.com>
2025-03-14 14:00:09 +0100

    add some new seeds

---
+++ src/database/seeders/seed.js
@@ -0,0 +1,79 @@
+import { hashSync } from 'bcryptjs';
+
+export default {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert(
+      'Categories',
+      [
+        {
+          name: 'Tech',
+          description: 'All things related to technology.',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Health',
+          description: 'Health and fitness',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      { returning: true }
+    );
+
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          email: 'admin@example.com',
+          password: hashSync('password123', 10),
+          userName: 'adminUser',
+          role: 'admin',
+        },
+        {
+          email: 'user@example.com',
+          password: hashSync('password123', 10),
+          userName: 'regularUser',
+          role: 'user',
+        },
+      ],
+      { returning: true }
+    );
+
+    await queryInterface.bulkInsert('Surveys', [
+      {
+        title: 'Tech Trends 2025',
+        status: 'draft',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Health Awareness Survey',
+        status: 'published',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+
+    const surveys = await queryInterface.sequelize.query(
+      'SELECT id FROM "Surveys";',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    );
+
+    await queryInterface.bulkInsert('Questions', [
+      {
+        question: 'What is your favorite programming language?',
+        surveyId: surveys[0].id,
+      },
+      { question: 'Do you follow AI developments?', surveyId: surveys[0].id },
+      { question: 'How often do you exercise?', surveyId: surveys[1].id },
+    ]);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Users', {});
+    await queryInterface.bulkDelete('Categories', {});
+    await queryInterface.bulkDelete('Surveys', null, {});
+    await queryInterface.bulkDelete('Questions', null, {});
+  },
+};
